e410a4d2273af8ad1f4003934b3fe5a7961b99a1
John Fox <jfox@mcmaster.ca>
2025-06-23 13:13:01 -0400

    revert previous commit to pre-ortho branch

--- scratch/Predict.R
+++
@@ -1,8 +0,0 @@
-Predict <- function(model, newdata, se.fit=FALSE,  ...){
-  if (missing(newdata)){
-    predict(model, se.fit=se.fit, ...)
-  } else {
-    wald.result <- suppressWarnings(wald(model, L.=getX(model, data=newdata))[[1]]$estimate)
-    if (se.fit) wald.result[, "Estimate", "Std.Error"] else wald.result[, "Estimate"]
-  }
-}
